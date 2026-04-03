eefc70b134e2789ba558261a95b39f09bf1f15da
Jaena Kathryn Vergara <jaena@Jaenas-MacBook-Air.local>
2026-04-03 14:45:09 -0700

    Add contract display UI with new styling, slightly less ugly

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 5174,
     proxy: {
-      "/api": "http://localhost:8080",
+      "/api": {
+        target: "http://127.0.0.1:8080",
+        changeOrigin: true,
+      },
     },
   },
 });
